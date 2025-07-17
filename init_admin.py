656d1ece4fafc1f86afbdad8c252aaa8adc93e7e
Abdallah-AlThwieb <aldwybballh@gmail.com>
2025-07-17 11:12:49 +0300

    Add admin creation script

--- init_admin.py
+++ init_admin.py
@@ -4,12 +4,12 @@ from models import db, Student
 from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.environ.get("DATABASE_USERNAME")}:{os.environ.get("DATABASE_PASSWORD")}@localhost/{os.environ.get("DATABASE_NAME")}'
+app.config['SQLALCHEMY_DATABASE_URI'] ="postgresql://athr2_user:9XE37PDe8UfxHbwwVvS915s9C8XCfYyy@dpg-d1sa7abipnbc73ds48sg-a.oregon-postgres.render.com/athr2"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 with app.app_context():
-    existing = Student.query.filter_by(username='admin').first()
+    existing = Student.query.filter_by(full_name='admin').first()
     if not existing:
         admin = Student(
             email='aldwybballh@gmail.com',  # ✅ إضافة البريد
