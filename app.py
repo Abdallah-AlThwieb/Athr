e8da841ab324912f391cb68fb8978dc911dd1d8c
Abdallah-AlThwieb <aldwybballh@gmail.com>
2025-07-17 10:56:16 +0300

    app.py Render

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ app.register_blueprint(survey)
 app.register_blueprint(admin_bp)
 
 # إنشاء الجداول تلقائيًا
-#with app.app_context():
-#  db.create_all()
+with app.app_context():
+  db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
