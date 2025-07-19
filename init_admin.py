561b73b15f3d15aea4d93b69a473e1d72abc7e3b
Abdallah-AlThwieb <aldwybballh@gmail.com>
2025-07-19 18:02:01 +0300

    project Athr update13

--- init_admin.py
+++ init_admin.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =f'postgresql://{os.environ.get("DATABASE_USERNAME")}:{os.environ.get("DATABASE_PASSWORD")}@localhost/{os.environ.get("DATABASE_NAME")}'
+app.config['SQLALCHEMY_DATABASE_URI'] =os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
@@ -14,9 +14,9 @@ with app.app_context():
     existing = Student.query.filter_by(full_name='admin').first()
     if not existing:
         admin = Student(
-            email=os.environ.get('OWNER_EMAIL'),  # ✅ إضافة البريد
-            full_name=os.environ.get('OWNER_FULL_NAME'),
-            password=generate_password_hash(os.environ.get('OWNER_PASSWORD')),
+            email="Fisal@gmail.com",  # ✅ إضافة البريد
+            full_name="فيصل صلاح الحربي",
+            password=generate_password_hash("Fisal2025"),
             is_admin=True
         )
         db.session.add(admin)
