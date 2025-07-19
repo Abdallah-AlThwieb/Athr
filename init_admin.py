acce910e672cea22cacbe2a0bd23be7c87c9ec57
Abdallah-AlThwieb <aldwybballh@gmail.com>
2025-07-19 18:51:32 +0300

    project Athr update15

--- init_admin.py
+++ init_admin.py
@@ -1,26 +1,21 @@
 import os
-from flask import Flask
 from models import db, Student
 from werkzeug.security import generate_password_hash
 from dotenv import load_dotenv
+from app import app  # ✅ استيراد الكائن الجاهز من app.py
 load_dotenv()
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =os.environ.get("DATABASE_URL")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
-
 with app.app_context():
-    existing = Student.query.filter_by(full_name='admin').first()
+    existing = Student.query.filter_by(full_name="admin").first()
     if not existing:
         admin = Student(
-            email="Fisal@gmail.com",  # ✅ إضافة البريد
+            email="Fisal@gmail.com",
             full_name="فيصل صلاح الحربي",
             password=generate_password_hash("Fisal2025"),
             is_admin=True
         )
         db.session.add(admin)
         db.session.commit()
-        print("✅ تم إنشاء حساب المشرف")
+        print("✅ تم إنشاء حساب المشرف بنجاح.")
     else:
         print("⚠️ حساب المشرف موجود بالفعل.")
