6980b4474057a55f581dc37a63e917b63075b73c
Abdallah-AlThwieb <aldwybballh@gmail.com>
2025-07-19 07:52:03 +0300

    project Athr update9

--- init_admin.py
+++ init_admin.py
@@ -2,9 +2,11 @@ import os
 from flask import Flask
 from models import db, Student
 from werkzeug.security import generate_password_hash
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ="postgresql://athr2_user:9XE37PDe8UfxHbwwVvS915s9C8XCfYyy@dpg-d1sa7abipnbc73ds48sg-a.oregon-postgres.render.com/athr2"
+app.config['SQLALCHEMY_DATABASE_URI'] =f'postgresql://{os.environ.get("DATABASE_USERNAME")}:{os.environ.get("DATABASE_PASSWORD")}@localhost/{os.environ.get("DATABASE_NAME")}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
@@ -12,13 +14,13 @@ with app.app_context():
     existing = Student.query.filter_by(full_name='admin').first()
     if not existing:
         admin = Student(
-            email='aldwybballh@gmail.com',  # ✅ إضافة البريد
-            full_name='عبدالله الذويب',
-            password=generate_password_hash('abdallah10'),
+            email=os.environ.get('OWNER_EMAIL'),  # ✅ إضافة البريد
+            full_name=os.environ.get('OWNER_FULL_NAME'),
+            password=generate_password_hash(os.environ.get('OWNER_PASSWORD')),
             is_admin=True
         )
         db.session.add(admin)
         db.session.commit()
-        print("✅ تم إنشاء حساب المشرف (abdallah / abdallah10)")
+        print("✅ تم إنشاء حساب المشرف")
     else:
         print("⚠️ حساب المشرف موجود بالفعل.")
